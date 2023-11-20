d1d099888fba5a6b3a00cba6c028071cde4190ea
hilim9 <dlagydls1212@naver.com>
2023-11-20 11:05:58 +0900

    초기 설정

---
+++ reactproject_be/src/main/java/org/lhi/reactproject_be/ReactprojectBeApplication.java
@@ -0,0 +1,13 @@
+package org.lhi.reactproject_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactprojectBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactprojectBeApplication.class, args);
+	}
+
+}
---
+++ reactproject_be/src/test/java/org/lhi/reactproject_be/ReactprojectBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lhi.reactproject_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactprojectBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
